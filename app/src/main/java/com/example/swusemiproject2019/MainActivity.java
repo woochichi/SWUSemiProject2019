495276ba83a402aadc4b33a70493ba2e7c130be7
tjdgml0401@swu.ac.kr <tjdgml0401@swu.ac.kr>
2019-07-03 14:55:15 +0900

    첫번째 나의 커밋이다.

--- app/src/main/java/com/example/swusemiproject2019/MainActivity.java
+++ app/src/main/java/com/example/swusemiproject2019/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //테스트 코드1
+
+        //오늘 컨디션 ㅎㅎ
+
+        //커밋 및 push 테스트
     }
 }
